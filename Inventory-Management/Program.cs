18f2cdf0e734f59170cbaade7cb40ddab4bbb968
Thyge123 <95471753+Thyge123@users.noreply.github.com>
2025-04-02 14:02:57 +0200

    Add CSV handling and database integration
    Added synthetic_online_retail_data.csv for order data.
    Created CsvRetailRecord class for CSV structure.
    Implemented RetailDataParser for parsing and saving data to the database.

--- Inventory-Management/Program.cs
+++ Inventory-Management/Program.cs
@@ -1,4 +1,5 @@
 using Inventory_Management.Context;
+using Inventory_Management.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,7 +23,16 @@ using (var db = new InventoryDbContext(builder.Services.BuildServiceProvider().G
     Console.WriteLine("Connected to the database successfully!");
 }
 
+builder.Services.AddScoped<RetailDataParser>();
+
+// Uncomment the following code to parse and save the data from the CSV file into the database
+
+// RetailDataParser retailDataParser = builder.Services.BuildServiceProvider().GetRequiredService<RetailDataParser>();
+
+// retailDataParser.ParseAndSaveData("C:\\Users\\spac-25\\source\\repos\\Inventory-Management\\Inventory-Management\\synthetic_online_retail_data.csv");
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
