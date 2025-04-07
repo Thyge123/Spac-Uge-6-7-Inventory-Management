5a6b394a9625f1a5c14b3e103f7d590bcc2e84c3
Thyge123 <95471753+Thyge123@users.noreply.github.com>
2025-04-07 10:10:39 +0200

    Add Transaction class with status enumeration.

---
+++ Inventory-Management/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿namespace Inventory_Management.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        
+        public enum Status
+        {
+            Sale,
+            Return,
+            Exchange
+        }
+
+        public Status TransactionStatus { get; set; }
+    }
+}
